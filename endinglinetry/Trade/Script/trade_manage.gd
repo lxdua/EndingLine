f4c641f32de4d4ac3daed690337e71dcadfc1f68
白头乌鸦 <115483162+baitouwuya@users.noreply.github.com>
2024-09-22 23:07:59 +0800

    1

--- endinglinetry/Trade/Script/trade_manage.gd
+++ endinglinetry/Trade/Script/trade_manage.gd
@@ -1,2 +1,40 @@
 extends Node2D
 class_name TradeManage
+@onready var player_trade_goods: TradeGoods = $PlayerTradeGoods
+
+
+var goods_data:Array[Dictionary]
+
+
+func _ready() -> void:
+	load_csv("res://goods_data_table.csv")
+	print(goods_data)
+
+
+func Trace(t_goods:TradeGoods):
+
+
+
+
+
+
+func load_csv(csv_path:String):
+	var csv_file
+	var items_title:PackedStringArray
+
+	if FileAccess.file_exists(csv_path):
+		csv_file = FileAccess.open(csv_path,FileAccess.READ)
+		items_title = csv_file.get_csv_line(",")
+
+		while csv_file.get_position()<csv_file.get_length():
+			var csv_item:PackedStringArray = csv_file.get_csv_line(",")
+			var item:Dictionary
+			for title in items_title.size():
+				if title == 0:
+					item[items_title[title]] = csv_item[title].to_int()
+				elif title == 2:
+					item[items_title[title]] = csv_item[title].to_float()
+				else:
+					item[items_title[title]] = csv_item[title]
+			goods_data.append(item)
+		csv_file.close()
