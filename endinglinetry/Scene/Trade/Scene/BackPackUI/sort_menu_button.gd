e4108a12e7912ec14a7b7bdaec72185d5a5c32bd
白头乌鸦 <115483162+baitouwuya@users.noreply.github.com>
2024-10-07 22:02:20 +0800

    1

--- endinglinetry/Scene/Trade/Scene/BackPackUI/sort_menu_button.gd
+++ endinglinetry/Scene/Trade/Scene/BackPackUI/sort_menu_button.gd
@@ -5,6 +5,7 @@ class_name SortMenuButton
 func _ready() -> void:
 	get_popup().id_pressed.connect(_popup_pressed)
 	text=get_popup().get_item_text(0)
+	print("S:",get_tree().current_scene)
 
 func _popup_pressed(id:int):
 	text=get_popup().get_item_text(id)
